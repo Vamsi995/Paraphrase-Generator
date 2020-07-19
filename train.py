9c04efe908c8e593f8bdcfb58ed11fb62b516dcd
Vamsi995 <saivamsi.ds123@gmail.com>
2020-07-19 12:53:54 +0530

    Adding the training script to fine tune T5

---
+++ train.py
@@ -0,0 +1,244 @@
+import argparse
+import glob
+import os
+import json
+import time
+import logging
+import random
+import re
+from itertools import chain
+from string import punctuation
+
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
+
+import pandas as pd
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pytorch_lightning as pl
+
+
+from transformers import AdamW, T5ForConditionalGeneration, T5Tokenizer, get_linear_schedule_with_warmup
+
+
+def set_seed(seed):
+  random.seed(seed)
+  np.random.seed(seed)
+  torch.manual_seed(seed)
+  if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(seed)
+
+set_seed(42)
+
+
+class T5FineTuner(pl.LightningModule):
+
+    def __init__(self, hparams):
+
+        # Calling the super constructer
+        super(T5FineTuner, self).__init__()
+
+        self.hparams = hparams
+
+        self.model = T5ForConditionalGeneration.from_pretrained(hparams.model_name_or_path)
+        self.tokenizer = T5Tokenizer.from_pretrained(hparams.tokenizer_name_or_path)
+
+    def forward(self, input_ids, attention_mask=None, decoder_input_ids=None, decoder_attention_mask=None,
+                lm_labels=None):
+
+        return self.model(input_ids, attention_mask=attention_mask,
+                          decoder_input_ids=decoder_input_ids,
+                          decoder_attention_mask=decoder_attention_mask,
+                          lm_labels=lm_labels, )
+
+    def is_logger(self):
+        return self.trainer.proc_rank <= 0
+
+    def _step(self, batch):
+        lm_labels = batch["target_ids"]
+        lm_labels[lm_labels[:, :] == self.tokenizer.pad_token_id] = -100
+
+        outputs = self(
+            input_ids=batch["source_ids"],
+            attention_mask=batch["source_mask"],
+            lm_labels=lm_labels,
+            decoder_attention_mask=batch['target_mask']
+        )
+
+        loss = outputs[0]
+
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        loss = self._step(batch)
+
+        tensorboard_logs = {"train_loss": loss}
+        return {"loss": loss, "log": tensorboard_logs}
+
+    def training_epoch_end(self, outputs):
+        avg_train_loss = torch.stack([x["loss"] for x in outputs]).mean()
+        tensorboard_logs = {"avg_train_loss": avg_train_loss}
+        return {"avg_train_loss": avg_train_loss, "log": tensorboard_logs, 'progress_bar': tensorboard_logs}
+
+    def validation_step(self, batch, batch_idx):
+        loss = self._step(batch)
+        return {"val_loss": loss}
+
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+        tensorboard_logs = {"val_loss": avg_loss}
+        return {"avg_val_loss": avg_loss, "log": tensorboard_logs, 'progress_bar': tensorboard_logs}
+
+    def configure_optimizers(self):
+        "Prepare optimizer and schedule (linear warmup and decay)"
+
+        model = self.model
+        no_decay = ["bias", "LayerNorm.weight"]
+        optimizer_grouped_parameters = [
+            {
+                "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+                "weight_decay": self.hparams.weight_decay,
+            },
+            {
+                "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+                "weight_decay": 0.0,
+            },
+        ]
+        optimizer = AdamW(optimizer_grouped_parameters, lr=self.hparams.learning_rate, eps=self.hparams.adam_epsilon)
+        self.opt = optimizer
+        return [optimizer]
+
+    def optimizer_step(self, epoch, batch_idx, optimizer, optimizer_idx, second_order_closure=None,
+                       using_native_amp=None):
+        if self.trainer.use_tpu:
+            xm.optimizer_step(optimizer)
+        else:
+            optimizer.step()
+        optimizer.zero_grad()
+        self.lr_scheduler.step()
+
+    def get_tqdm_dict(self):
+        tqdm_dict = {"loss": "{:.3f}".format(self.trainer.avg_loss), "lr": self.lr_scheduler.get_last_lr()[-1]}
+
+        return tqdm_dict
+
+    def train_dataloader(self):
+        train_dataset = CustomDataset(tokenizer=self.tokenizer, type_path="PAW_Train_Global",
+                                      data_dir=self.hparams.data_dir, max_len=self.hparams.max_seq_length)
+        dataloader = DataLoader(train_dataset, batch_size=self.hparams.train_batch_size, drop_last=True, shuffle=True,
+                                num_workers=4)
+        t_total = (
+                (len(dataloader.dataset) // (self.hparams.train_batch_size * max(1, self.hparams.n_gpu)))
+                // self.hparams.gradient_accumulation_steps
+                * float(self.hparams.num_train_epochs)
+        )
+        scheduler = get_linear_schedule_with_warmup(
+            self.opt, num_warmup_steps=self.hparams.warmup_steps, num_training_steps=t_total
+        )
+        self.lr_scheduler = scheduler
+        return dataloader
+
+
+
+
+    def val_dataloader(self):
+        val_dataset = CustomDataset(tokenizer=self.tokenizer, type_path="PAW_Test_Global",
+                                    data_dir=self.hparams.data_dir, max_len=self.hparams.max_seq_length)
+        return DataLoader(val_dataset, batch_size=self.hparams.eval_batch_size, num_workers=4)
+
+
+logger = logging.getLogger(__name__)
+
+class LoggingCallback(pl.Callback):
+  def on_validation_end(self, trainer, pl_module):
+    logger.info("***** Validation results *****")
+    if pl_module.is_logger():
+      metrics = trainer.callback_metrics
+      # Log results
+      for key in sorted(metrics):
+        if key not in ["log", "progress_bar"]:
+          logger.info("{} = {}\n".format(key, str(metrics[key])))
+
+  def on_test_end(self, trainer, pl_module):
+    logger.info("***** Test results *****")
+
+    if pl_module.is_logger():
+      metrics = trainer.callback_metrics
+
+      # Log and save results to file
+      output_test_results_file = os.path.join(pl_module.hparams.output_dir, "test_results.txt")
+      with open(output_test_results_file, "w") as writer:
+        for key in sorted(metrics):
+          if key not in ["log", "progress_bar"]:
+            logger.info("{} = {}\n".format(key, str(metrics[key])))
+            writer.write("{} = {}\n".format(key, str(metrics[key])))
+
+
+
+# Hyper parameters
+args_dict = dict(
+    data_dir="", # path for data files
+    output_dir="", # path to save the checkpoints
+    model_name_or_path='t5-base',
+    tokenizer_name_or_path='t5-base',
+    max_seq_length=512,
+    learning_rate=3e-4,
+    weight_decay=0.0,
+    adam_epsilon=1e-8,
+    warmup_steps=0,
+    train_batch_size=6,
+    eval_batch_size=6,
+    num_train_epochs=2,
+    gradient_accumulation_steps=16,
+    n_gpu=1,
+    early_stop_callback=False,
+    fp_16=False, # if you want to enable 16-bit training then install apex and set this to true
+    opt_level='O1', # you can find out more on optimisation levels here https://nvidia.github.io/apex/amp.html#opt-levels-and-properties
+    max_grad_norm=1.0, # if you enable 16-bit training then set this to a sensible value, 0.5 is a good default
+    seed=42,
+)
+
+
+
+
+
+if not os.path.exists('t5_paw_global'):
+    os.makedirs('t5_paw_global')
+
+args_dict.update({'output_dir': 't5_paw_global','num_train_epochs':1,'max_seq_length':256})
+args = argparse.Namespace(**args_dict)
+print(args_dict)
+
+
+checkpoint_callback = pl.callbacks.ModelCheckpoint(
+    filepath=args.output_dir, prefix="checkpoint", monitor="val_loss", mode="min", save_top_k=5
+)
+
+train_params = dict(
+    accumulate_grad_batches=args.gradient_accumulation_steps,
+    gpus=args.n_gpu,
+    max_epochs=args.num_train_epochs,
+    early_stop_callback=False,
+    precision= 16 if args.fp_16 else 32,
+    amp_level=args.opt_level,
+    gradient_clip_val=args.max_grad_norm,
+    checkpoint_callback=checkpoint_callback,
+    callbacks=[LoggingCallback()],
+)
+
+
+model = T5FineTuner(args)
+
+trainer = pl.Trainer(**train_params)
+
+print (" Training model")
+trainer.fit(model)
+
+print ("training finished")
+
+print ("Saving model")
+model.model.save_pretrained('t5_paw_global')
+
+print ("Saved model")
