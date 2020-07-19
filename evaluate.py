d4ba8e55e25febe1f5f087dcbdbfa7ec9bdea931
Vamsi995 <saivamsi.ds123@gmail.com>
2020-07-19 13:09:14 +0530

    Evalute.py generates the sentences

---
+++ evaluate.py
@@ -0,0 +1,43 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+model = AutoModelForSeq2SeqLM.from_pretrained("Vamsi/T5_Paraphrase_Paws")
+tokenizer = AutoTokenizer.from_pretrained("Vamsi/T5_Paraphrase_Paws")
+
+model.to("cuda")
+
+sentence = "This is something which i cannot understand at all"
+
+text =  "paraphrase: " + sentence + " </s>"
+
+
+max_len = 256
+
+encoding = tokenizer.encode_plus(text,pad_to_max_length=True, return_tensors="pt")
+input_ids, attention_masks = encoding["input_ids"].to("cuda"), encoding["attention_mask"].to("cuda")
+
+
+# set top_k = 50 and set top_p = 0.95 and num_return_sequences = 3
+# Have to read about these decodings
+beam_outputs = model.generate(
+    input_ids=input_ids, attention_mask=attention_masks,
+    do_sample=True,
+    max_length=256,
+    top_k=220,
+    top_p=1,
+    early_stopping=True,
+    num_return_sequences=5
+)
+
+
+print ("\nOriginal Question ::")
+print (sentence)
+print ("\n")
+print ("Paraphrased Questions :: ")
+final_outputs =[]
+for beam_output in beam_outputs:
+    sent = tokenizer.decode(beam_output, skip_special_tokens=True,clean_up_tokenization_spaces=True)
+    if sent.lower() != sentence.lower() and sent not in final_outputs:
+        final_outputs.append(sent)
+
+for i, final_output in enumerate(final_outputs):
+    print("{}: {}".format(i, final_output))
